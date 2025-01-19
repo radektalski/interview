c768e5fbb011e75c285d3f1976ae820b8c233cb6
wartalski_wh <radoslaw.wartalski1@grandparade.co.uk>
2025-01-19 11:33:23 +0100

    fix eng

--- cypress/e2e/api-tests.cy.ts
+++ cypress/e2e/api-tests.cy.ts
@@ -3,9 +3,9 @@
 Write an API test with Cypress that will GET data from https://api.thedogapi.com/v1/breeds?limit=10&page=0 
 with the API header: x-api-key: {api-key-provided-by-interviewer}
 
-Perform the following assertions on the response:
+Perform the following actions on the response:
 1. Ensure that response status is 200 and there are 10 items in the response.
-2. From te reponse list of objects create an array of breen names and print it to the console as string separated by comma.
+2. From the response's list of objects, create an array of breed names and print it to the console as a string separated by commas.
 
 */
 
